ff9314f08d1f2c98bd7bb9bc920bfdcc37186598
Nick <Echophase44@github.com>
2022-07-15 22:08:26 -0400

    Added express server

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,2 +1,15 @@
 //create game instance
 // Structure: playgame -> opening prompts -> create game board -> player 1 and player 2 setup -> board loop (toggle players, detect for win conditions)
+
+class Game {
+  constructor() {}
+  intro() {
+    console.log(
+      "Welcome to Tic-Tac-Toe. Would you like to play a new game? y/n"
+    );
+  }
+}
+
+let gameOne = new Game();
+
+gameOne.intro();
