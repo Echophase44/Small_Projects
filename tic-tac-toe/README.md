42ea6bc96f628c93fff91531e4e1278f5d6e1cef
Nick <Echophase44@github.com>
2022-10-22 07:24:17 -0400

    Adding some pseudo-code to lay out some game logic

--- tic-tac-toe/README.md
+++ tic-tac-toe/README.md
@@ -2,4 +2,6 @@
 
 ## Goals
 
+- To utilize IIFE, objects, and factory function patterns to build out a tic-tac-toe game.
+
 ## Notes
--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,15 +1,9 @@
-//create game instance
-// Structure: playgame -> opening prompts -> create game board -> player 1 and player 2 setup -> board loop (toggle players, detect for win conditions)
-
-class Game {
-  constructor() {}
-  intro() {
-    console.log(
-      "Welcome to Tic-Tac-Toe. Would you like to play a new game? y/n"
-    );
-  }
-}
-
-let gameOne = new Game();
-
-gameOne.intro();
+//Play Game
+//Create Player 1
+//Assign X
+//Create Player 2
+//Assign O
+//Create Game
+//Switch between players
+//Have each player build their own array
+//Compare each array against winning arrays
