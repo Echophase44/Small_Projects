fb71dac87dc74e87f6189aeeae0ce1a04291140a
Nick <Echophase44@github.com>
2022-10-23 23:03:33 -0400

    Added some functionality to the start button to initialize game and got X working in populating the cells

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -5,5 +5,23 @@
 //Assign O
 //Create Game
 //Switch between players
-//Have each player build their own array
+//Have each player build their own array to compare against winning arrays
 //Compare each array against winning arrays
+
+// ######################### Click to Start Game#########################
+const playBtn = document.querySelector("#playBtn");
+playBtn.addEventListener("click", () => {
+  playGame();
+});
+
+// ######################### Main Game #########################
+const playGame = () => {
+  const cell = document.querySelectorAll(".cell");
+  cell.forEach((element) => {
+    element.addEventListener("click", () => {
+      element.textContent = "X";
+    });
+  });
+
+  // const resetGame()
+};
